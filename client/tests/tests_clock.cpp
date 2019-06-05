ecf53da4cd6cffde7deda0b7ee2b4cc7e4778b1d
Arthur Dassier <35168820+ArthurDassier@users.noreply.github.com>
2019-06-05 17:35:03 +0200

    [ADD] Unit test Server / Client & CI (#5)

---
+++ client/tests/tests_clock.cpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2019
+** tests_clock.cpp
+** File description:
+** tests_clock
+*/
+
+#include "gtest/gtest.h"
+#include "../include/client/Irrlicht.hpp"
+
+TEST(TestIrrlicht, getDataNULL)
+{
+    EngineGraphic graph;
+
+    ASSERT_EQ(graph.getData(), "");
+}
+
+TEST(TestIrrlicht, getData)
+{
+    EngineGraphic graph;
+
+    graph.dataMove("up");
+    ASSERT_NE(graph.getData(), "");
+}
+
+TEST(TestIrrlicht, clearData)
+{
+    EngineGraphic graph;
+
+    graph.dataMove("up");
+    graph.clearData();
+    ASSERT_EQ(graph.getData(), "");
+}
