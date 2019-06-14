327e43f88ec01e3ee200cb7df7bd714c2a1a36f9
Charles Raimbault <charles.raimbault@epitech.eu>
2019-06-14 20:30:32 +0200

    [FIX] visuel

--- client/srcs/Graphic.cpp
+++ client/srcs/Graphic.cpp
@@ -11,7 +11,7 @@ client::EngineGraphic::EngineGraphic():
     _receiver(),
     _device(
         irr::createDevice(
-            video::EDT_SOFTWARE,
+            video::EDT_OPENGL,
             core::dimension2d<u32>(960,540),
             16,
             false,
@@ -95,7 +95,10 @@ void client::EngineGraphic::addEntity(Character *player)
 
 scene::ICameraSceneNode *client::EngineGraphic::addCamera()
 {
-    return (_smgr->addCameraSceneNode(0, core::vector3df(22,71,-20), core::vector3df(35,25,10)));
+    irr::core::array<irr::scene::ISceneNode *>	nodes;
+    _smgr->loadScene("map.irr");
+    _smgr->getSceneNodesFromType(irr::scene::ESNT_CUBE, nodes);
+    return (_smgr->addCameraSceneNode(0, core::vector3df(0,150,-20), core::vector3df(0,0,0)));
 }
 
 void client::EngineGraphic::moveEntity(std::string sens, std::string id)
@@ -239,19 +242,20 @@ void client::EngineGraphic::explosion()
 
 void client::EngineGraphic::dropBonus()
 {
-    // std::string bonusType = _root.get<std::string>("bonusType");
-    // core::vector3df pos(_root.get<float>("x"), 5, _root.get<float>("z"));
-    // scene::IAnimatedMesh* mesh = _smgr->getMesh("client/res/Bomb.3ds");
-    // // scene::IAnimatedMesh* mesh = _smgr->getMesh("client/res/" + bonusType + ".3ds");
-    // scene::IAnimatedMeshSceneNode *node = _smgr->addAnimatedMeshSceneNode(mesh);
-    // // node->setMaterialTexture(0, _driver->getTexture("client/res/" + bonusType + ".png"));
-    // node->setMaterialTexture(0, _driver->getTexture("client/res/Albedo.png"));
-    // node->setRotation(core::vector3df(0, 80, 0));
-    // node->setPosition(pos);
-    // node->setFrameLoop(0, 0);
-    // node->setScale(core::vector3df(30, 30, 30));
-    // node->setMaterialFlag(video::EMF_LIGHTING, false);
-    // _nodeBonus.push_back(node);
+    float x = _root.get<float>("x");
+    float z = _root.get<float>("z");
+    std::string bonusType = _root.get<std::string>("bonusType");
+    if (bonusType.compare("SpeedUp") != 0)
+        return;
+    std::cout << "rose\n";
+    core::vector3df pos(20, 20, 20);
+    scene::IAnimatedMeshSceneNode *node = _smgr->addAnimatedMeshSceneNode(_loader.getModel("rose"));
+    node->setRotation(core::vector3df(0, 80, 0));
+    node->setPosition(pos);
+    node->setFrameLoop(0, 0);
+    node->setScale(core::vector3df(100, 100, 100));
+    node->setMaterialFlag(video::EMF_LIGHTING, false);
+    _nodeBonus.push_back(node);
 }
 
 void client::EngineGraphic::removeBonus()
@@ -314,7 +318,7 @@ void client::EngineGraphic::bomb()
     node->setRotation(core::vector3df(0, 80, 0));
     node->setPosition(pos);
     node->setFrameLoop(0, 0);
-    node->setScale(core::vector3df(10, 10, 10));
+    node->setScale(core::vector3df(30, 30, 30));
     node->setMaterialFlag(video::EMF_LIGHTING, false);
     _nodeBomb.push_back(node);
 }
---
+++ game/srcs/SpeedUp.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** IndieStudio_2018
+** File description:
+** BombUp.cpp
+*/
+
+#include "Game/SpeedUp.hpp"
+
+game::Speedup::Speedup():
+    Entity(game::BombUp)
+{
+}
